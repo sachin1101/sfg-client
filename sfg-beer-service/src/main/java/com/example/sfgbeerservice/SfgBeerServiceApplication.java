0c9dc6366bfb56b58b2fd6add2bf77f7c50d4ef5
user01 <sachin.saraswat1741@gmail.com>
2019-09-03 18:57:26 -0400

    Closes #1

---
+++ sfg-beer-service/src/main/java/com/example/sfgbeerservice/SfgBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgBeerServiceApplication.class, args);
+    }
+
+}
---
+++ sfg-beer-service/src/test/java/com/example/sfgbeerservice/SfgBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
